d6ed3dbc4d8b5063af7c458453b098bd6814f139
Boris Sheludko <bs.sheludko@gmail.com>
2020-11-16 23:16:39 +0000

    added 09 task

---
+++ 09_I18nL10n/guess.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <libintl.h>
+#include <locale.h>
+#include <stdlib.h>
+
+#define _(STRING) gettext(STRING)
+
+int main() {
+    setlocale (LC_ALL, "");
+    char *ss = bindtextdomain("guess", ".");
+    textdomain("guess");
+
+    char msg[10];
+    int l = 1, r = 100, flag;
+    printf(_("Come up with a number from %d to %d\n"), l, r);
+    while(l < r) {
+        int m = (int)((l + r) / 2);
+        printf(_("Is this number greater than %d or not?\n"), m);
+        scanf("%s", msg);
+        if(strcmp("yes", msg) == 0) {
+            flag = 0;
+        } else if(strcmp(msg, "no") == 0) {
+            flag = 1;
+        } else {
+            printf(_("Incorrect value %s - type yes or no\n"), msg);
+            continue;
+        }
+        if(flag) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+    }
+    printf(_("Your number is %d\n"), l);
+
+    return 0;
+}
